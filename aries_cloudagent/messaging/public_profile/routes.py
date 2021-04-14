c8a85f99b0c0dd05b265e3fda2b5e2570b5dc46d
Matthias Binzer <matthias.binzer@de.bosch.com>
2021-04-14 18:23:41 +0200

    Given credentials are signed as VP
    TODO:
    - ProofType is not recognized yet
    - A Verify method for the new proof type is missing
    - How to get network specific did prefix?

--- aries_cloudagent/messaging/public_profile/routes.py
+++ aries_cloudagent/messaging/public_profile/routes.py
@@ -1,5 +1,8 @@
 """Public Profile admin routes."""
 
+import json
+from pyld import jsonld
+
 from aiohttp import web
 from aiohttp_apispec import docs, request_schema, response_schema
 from aiohttp_apispec.decorators import response
@@ -10,6 +13,8 @@ from ...admin.request_context import AdminRequestContext
 
 from ...wallet.base import BaseWallet
 from ...indy.holder import IndyHolder, IndyHolderError
+
+from ..jsonld.credential import sign_credential, verify_credential
 
 from ..models.openapi import OpenAPISchema
 
@@ -34,19 +39,66 @@ class CreatePublicProfileResponseSchema(OpenAPISchema):
 async def create_public_profile(request: web.BaseRequest):
     vcs = []
     context: AdminRequestContext = request["context"]
-    credential_ids = request.match_info["credential_ids"]
-    verkey = request.match_info["verkey"]
+    body = await request.json()
+    verkey = body.get("verkey")
+    credential_ids = body.get("credential_ids")
     session = await context.session()
+
+    wallet = session.inject(BaseWallet, required=False)
+    public_did_obj = await wallet.get_public_did()
 
     holder = session.inject(IndyHolder)
     for id in credential_ids:
         credential = await holder.get_credential(id)
         credential_json = json.loads(credential)
-        vcs.append(credential_json)
+        w3c_vc = build_w3c_credential(credential_json)
+        vcs.append(w3c_vc)
     profile = {}
-    profile["verifiableCredentials"] = vcs
-    return web.json_response(profile)
+    vp_context = [
+        "https://www.w3.org/2018/credentials/v1"
+    ]
+    profile["@context"] = vp_context
+    profile["type"] = "VerifiablePresentation"
+    profile["verifiableCredential"] = vcs
 
+    sig_options = {
+        "verificationMethod": get_network_did_prefix() + public_did_obj.did,
+        "proofPurpose": "assertionMethod"
+    }
+
+    compacted = jsonld.compact(profile, vp_context)
+    print(json.dumps(compacted, indent=2))
+    signed_profile = await sign_credential(profile, sig_options, public_did_obj.verkey, wallet)
+
+    return web.json_response(signed_profile)
+
+def build_w3c_credential(credential):
+    vc = {}
+
+    attrs = credential["attrs"]
+    cred_def_id = credential["cred_def_id"]
+    subject_context = {}
+    subject_context["sc"] = get_network_did_prefix() + cred_def_id
+    for k,v in attrs.items():
+        subject_context[k] = {
+            "@id": "sc:" + k
+        }
+    vc["@context"] = [
+        "https://www.w3.org/2018/credentials/v1",
+        {"@context": subject_context }
+    ]
+    vc["type"] = "VerifiableCredential"
+    vc["credentialSubject"] = attrs
+    proof_type = {
+        "type": "IndyCredDefProofType",
+        "proofPurpose": "assertionMethod",
+        "verificationMethod": get_network_did_prefix() + cred_def_id
+    }
+    vc["proof"] = proof_type
+    return vc
+
+def get_network_did_prefix():
+    return "did:sov:iil:"
 
 async def register(app: web.Application):
     """Register routes."""
