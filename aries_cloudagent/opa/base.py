9485142e16403e7cf77809ec4db3f2af6b99fd5f
Woerner Dominic (RBCH/PJ-IOT) <dominic.woerner2@ch.bosch.com>
2021-06-02 08:03:59 +0200

    present proof not working. State updating issues
    Signed-off-by: Woerner Dominic (RBCH/PJ-IOT) <dominic.woerner2@ch.bosch.com>

---
+++ aries_cloudagent/opa/base.py
@@ -0,0 +1,230 @@
+"""Open Policy Agent Plugin."""
+
+# import json
+
+import aiohttp
+import asyncio
+import logging
+import re
+
+
+from ..indy.holder import IndyHolder, IndyHolderError
+from ..indy.sdk.models.xform import indy_proof_req_preview2indy_requested_creds
+from ..ledger.error import LedgerError
+from ..messaging.responder import BaseResponder
+from ..messaging.models.base import BaseModelError
+from ..config.injection_context import InjectionContext
+from ..core.error import BaseError
+from ..core.event_bus import Event, EventBus
+from ..core.profile import Profile
+from ..indy.util import generate_pr_nonce
+from ..storage.error import StorageError, StorageNotFoundError
+from ..wallet.error import WalletNotFoundError
+
+from ..connections.models.conn_record import ConnRecord
+from ..messaging.decorators.attach_decorator import AttachDecorator
+from ..protocols.didexchange.v1_0.manager import DIDXManager
+from ..protocols.present_proof.v1_0.manager import PresentationManager
+from ..protocols.present_proof.v1_0.message_types import (
+    ATTACH_DECO_IDS,
+    PRESENTATION_REQUEST,
+)
+from ..protocols.present_proof.v1_0.messages.presentation_request import (
+    PresentationRequest,
+)
+from ..protocols.present_proof.v1_0.models.presentation_exchange import (
+    V10PresentationExchange,
+)
+
+# from ..admin.server import AdminServer
+
+LOGGER = logging.getLogger(__name__)
+
+EVENT_PATTERN_RECORD = re.compile("^acapy::record::([^:]*)(?:::.*)?$")
+
+
+class Opa:
+    """Open Policy Agent."""
+
+    def __init__(self, context: InjectionContext):
+        self._context = context
+        self._opa_base_url = "http://host.docker.internal:8181/v1/data/"
+        self._pres_req_id = {}
+
+        event_bus = self._context.inject(EventBus, required=False)
+        if event_bus:
+            event_bus.subscribe(EVENT_PATTERN_RECORD, self._on_record_event)
+
+    async def _on_record_event(self, profile: Profile, event: Event):
+        match = EVENT_PATTERN_RECORD.search(event.topic)
+        topic = match.group(1) if match else None
+        LOGGER.info("OPA Message received: %s", topic)
+
+        if topic == "present_proof":
+
+            if event.payload["state"] == "presentation_sent":
+                self._pres_req_id = {
+                    event.payload["presentation_exchange_id"]: "presentation_sent"
+                }
+
+            if (
+                self._pres_req_id.get(event.payload["presentation_exchange_id"], None)
+                == "presentation_sent"
+                and event.payload["state"] == "request_received"
+            ):
+                return
+
+            indy_proof_request = event.payload["presentation_request"]
+            session = await profile.session(self._context)
+
+            (
+                presentation_exchange_record
+            ) = await V10PresentationExchange.retrieve_by_tag_filter(
+                session,
+                {"thread_id": event.payload["thread_id"]},
+                {"connection_id": event.payload["connection_id"]},
+            )
+
+            presentation_manager = PresentationManager(profile)
+
+            presentation_exchange_record = await presentation_manager.receive_request(
+                presentation_exchange_record
+            )  # mgr only saves record: on exception, saving state null is hopeless
+            presentation_preview = None
+            if presentation_exchange_record.presentation_proposal_dict:
+                exchange_pres_proposal = (
+                    presentation_exchange_record.presentation_proposal_dict
+                )
+                presentation_preview = exchange_pres_proposal.presentation_proposal
+            req_creds = {}
+            try:
+                req_creds = await indy_proof_req_preview2indy_requested_creds(
+                    indy_proof_request,
+                    presentation_preview,
+                    holder=self._context.inject(IndyHolder),
+                )
+
+            except ValueError as err:
+                LOGGER.warning(f"{err}")
+                pass  # not a protocol error: prover could still build proof manually
+            event.payload["req_creds"] = req_creds
+        input_dict = {"input": event.payload}
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                self._opa_base_url + topic, json=input_dict
+            ) as response:
+                resp_json = await response.json()
+                LOGGER.info(resp_json)
+                action = resp_json["result"]["action"] if "result" in resp_json else []
+                action = action[0] if len(action) > 0 else None
+                if action:
+
+                    # Handle actions of type "connection"
+                    if action["type"] == "connection":
+                        if action["data"]["accept"]:
+                            session = await profile.session(self._context)
+                            didx_mgr = DIDXManager(session)
+                            conn_rec = await ConnRecord.retrieve_by_id(
+                                session, event.payload["connection_id"]
+                            )
+
+                            async def respond(didx_mgr, conn_rec, session):
+                                await asyncio.sleep(2)
+                                didx_resp = await didx_mgr.create_response(conn_rec)
+                                responder = session.inject(BaseResponder, required=True)
+                                if responder:
+                                    await responder.send_reply(
+                                        didx_resp, connection_id=conn_rec.connection_id
+                                    )
+
+                            loop = asyncio.get_event_loop()
+                            loop.create_task(respond(didx_mgr, conn_rec, session))
+
+                    # Handle actions of type "request proof"
+                    elif action["type"] == "request proof":
+                        LOGGER.info("Send proof request")
+                        session = await profile.session(self._context)
+                        connection_id = event.payload["connection_id"]
+
+                        conn_rec = await ConnRecord.retrieve_by_id(
+                            session, connection_id
+                        )
+
+                        indy_proof_request = action["data"]["proof_request"]
+                        if not indy_proof_request.get("nonce"):
+                            indy_proof_request["nonce"] = await generate_pr_nonce()
+
+                        presentation_request_message = PresentationRequest(
+                            request_presentations_attach=[
+                                AttachDecorator.data_base64(
+                                    mapping=indy_proof_request,
+                                    ident=ATTACH_DECO_IDS[PRESENTATION_REQUEST],
+                                )
+                            ],
+                        )
+
+                        presentation_manager = PresentationManager(profile)
+
+                        await presentation_manager.create_exchange_for_request(
+                            connection_id=connection_id,
+                            presentation_request_message=presentation_request_message,
+                        )
+                        responder = session.inject(BaseResponder, required=True)
+                        if responder:
+                            await responder.send(
+                                presentation_request_message,
+                                connection_id=connection_id,
+                            )
+
+                    elif action["type"] == "present proof":
+                        LOGGER.info("Present proof")
+                        connection_id = event.payload["connection_id"]
+                        req_creds["self_attested_attributes"] = action["data"][
+                            "self_attested_attr"
+                        ][0]
+                        req_creds["requested_attributes"] = {}
+                        req_creds["requested_predicates"] = {}
+                        session = await profile.session(self._context)
+                        responder = session.inject(BaseResponder, required=True)
+                        presentation_message = None
+                        try:
+                            (
+                                presentation_exchange_record,
+                                presentation_message,
+                            ) = await presentation_manager.create_presentation(
+                                presentation_exchange_record=presentation_exchange_record,
+                                requested_credentials=req_creds,
+                                comment="auto-presented for proof request nonce={}".format(
+                                    indy_proof_request["nonce"]
+                                ),
+                            )
+                            await responder.send(
+                                presentation_message, connection_id=connection_id
+                            )
+                        except (
+                            BaseModelError,
+                            IndyHolderError,
+                            LedgerError,
+                            StorageError,
+                            WalletNotFoundError,
+                        ) as err:
+                            LOGGER.exception(err)
+                            if presentation_exchange_record:
+                                async with self._context.session() as session:
+                                    await presentation_exchange_record.save_error_state(
+                                        session, reason=err.roll_up,  # us: be specific
+                                    )
+
+                                # await responder.send_reply(
+                                #     problem_report_for_record(
+                                #         presentation_exchange_record,
+                                #         ProblemReportReason.ABANDONED.value,  # them: be vague
+                                #     )
+                                # )
+
+                    # Handle actions of type "tag connection"
+                    elif action["type"] == "tag connection":
+                        LOGGER.info("Tag connection")
+
+    async def setup(self, context: InjectionContext):
+        """Perform required setup for OPA Plugin."""
